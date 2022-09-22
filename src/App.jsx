395f1934c835b38d534e4b426421054d0ddb168e
ChrisPoul <chrispoulsenfernandez@gmail.com>
2022-09-22 12:57:52 -0500

    Refactord

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ export function App() {
   return (
     <>
       {DisplayItems(products, "Products")}
-      {AddItem("Product")}
     </>
   );
 }
--- src/components/addItem.jsx
+++ src/components/addItem.jsx
@@ -8,25 +8,43 @@ export function AddItem(heading) {
     const [name, setName] = useState("")
     const [price, setPrice] = useState(0)
 
-
     const handleSubmit = event => {
         event.preventDefault()
-        API.addProduct({
+        const response = API.addProduct({
             name: name,
             price: price
         })
-        alert("El producto fue añadido")
+        response.then(data => {
+            if (data == null) {
+                alert("Success")
+                setName("")
+                setPrice(0)
+            } else {
+                for (const key in data) {
+                    alert(data[key])
+                }
+            }
+        })
     }
     return (
         <Box>
             <form onSubmit={handleSubmit}>
                 <FormControl key="name" isRequired>
                     <FormLabel>Nombre</FormLabel>
-                    <Input name="name" onChange={event => setName(event.currentTarget.value)} />
+                    <Input
+                        name="name"
+                        value={name}
+                        onChange={event => setName(event.currentTarget.value)}
+                    />
                 </FormControl>
                 <FormControl key="price" isRequired>
                     <FormLabel>Precio</FormLabel>
-                    <Input name="price" type="number" onChange={event => setPrice(event.currentTarget.value)} />
+                    <Input
+                        name="price"
+                        type="number"
+                        value={price}
+                        onChange={event => setPrice(event.currentTarget.value)}
+                    />
                 </FormControl>
                 <Button type="submit">Submit</Button>
             </form>
