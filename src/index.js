e3dee80ef225bc1c1fba30c8a58983520b020d6e
ChrisPoul <chrispoulsenfernandez@gmail.com>
2022-10-25 07:55:13 -0500

    Removed general theme

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
-import { theme } from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider theme={theme}>
+  <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
